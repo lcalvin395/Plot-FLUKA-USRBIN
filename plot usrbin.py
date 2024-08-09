c7cf387c141fd0d316ae6be04ddb3627fd352642
lcalvin02 <lcalvin02@qub.ac.uk>
2024-08-09 17:16:10 +0100

    v

--- plot usrbin.py
+++ plot usrbin.py
@@ -1154,7 +1154,7 @@ for i in range(0,len(listofclustersize)):
     weights.append(1/len(listofclustersize))  
 fig, ax=plt.subplots()   
 binwidth=1
-plt.hist(listofclustersize, bins=range(int(min(listofclustersize)), int(max(listofclustersize) + binwidth)), width=1,colour='b')
+plt.hist(listofclustersize, bins=range(int(min(listofclustersize)), int(max(listofclustersize) + binwidth)), width=1,color='b')
 counts, bins, bars = plt.hist(listofclustersize, bins=range(int(min(listofclustersize)), int(max(listofclustersize) + binwidth)), width=1)
 plt.xlabel("Size of Cluster - pixels")
 plt.ylabel('N')
