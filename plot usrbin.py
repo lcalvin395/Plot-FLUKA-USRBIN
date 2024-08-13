39a002d0d8b9b3b548af2b8bb64d5c2b48836526
lcalvin02 <lcalvin02@qub.ac.uk>
2024-08-13 13:04:27 +0100

    coeff to standard dev

--- plot usrbin.py
+++ plot usrbin.py
@@ -2282,15 +2282,15 @@ binwidth=10
 fig, ax=plt.subplots()
 ax = plt.gca()
 #ax.set_ylim([0,110])
-plt.xlabel("Coefficient of Variation")
-plt.ylabel('N')
+plt.xlabel("Standard Deviation - KeV")
+plt.ylabel('%N')
 
 #ax.hist(totalenergyincluster, bins=150, weights=weights)
 #ax.hist(coeffvariation, bins=range(int(min(coeffvariation)), int(max(coeffvariation) + binwidth), binwidth), color='b')
 #ax.hist(coeffvariation, bins=(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5), color='b')
 #counts, bins, bars = ax.hist(coeffvariation, bins=(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5), color='b')
 ax.hist(coeffvariation, bins=range(int(min(coeffvariation)), int(max(coeffvariation) + binwidth), binwidth), color='b',weights=weights)
-counts, bins, bars =ax.hist(coeffvariation, bins=range(int(min(coeffvariation)), int(max(coeffvariation) + binwidth), binwidth), color='b',weights=weights)
+counts, bins, bars =ax.hist(coeffvariation, bins=range(int(min(coeffvariation)), int(max(coeffvariation) + binwidth), binwidth), color='b')
 plt.savefig('/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/{}_coeff_variation_in_cluster_hist.png'.format(particle1),bbox_inches='tight', dpi=1000)
 
 
