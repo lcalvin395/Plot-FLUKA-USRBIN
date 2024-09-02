69f5f461c9a690252533cb9b49abae5c73d55132
lcalvin02 <lcalvin02@qub.ac.uk>
2024-09-02 15:18:36 +0100

    d

--- plot usrbin.py
+++ plot usrbin.py
@@ -1901,6 +1901,8 @@ prob10=0
 for i in range(0,len(counts)):
     if bins[i]>=10:
         prob10+=counts[i]
+    if bins[i]==10:
+        print(counts[i])
 print('Probs of muon of Std Dev >= 10KeV', prob10)
 
 
