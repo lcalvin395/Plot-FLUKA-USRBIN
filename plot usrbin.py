d24baceb4e4f52c8dc19cfa5f8ff48f75ca753ed
lcalvin02 <lcalvin02@qub.ac.uk>
2024-10-01 15:37:41 +0100

    s

--- plot usrbin.py
+++ plot usrbin.py
@@ -2077,4 +2077,11 @@ meandiffs=np.mean(diffs)
 print('mean slope:', np.mean(slope))
 print('max slope: ', max(slope))
 print('min slope: ', min(slope))
+
+
+
+
+fig, ax=plt.subplots()   
+ax = plt.gca()
+ax.hist(slope, bins=(0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0),color='b')
 plt.show()
